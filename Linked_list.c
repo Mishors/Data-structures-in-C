b021bbb26f190e5f478ab7fb905ab265048d360f
mishors <mishors@yahoo.co.uk>
2018-04-05 19:09:01 +0200

    Numeric linked list

---
+++ Linked_list.c
@@ -0,0 +1,150 @@
+#include "linked_list.h"
+
+void print_list(struct node* head)
+{
+ if(head == NULL) return;
+ struct node* iter = head;
+ while(iter != NULL)
+ {
+ 	printf("%d ",iter->value);
+	iter = iter->next;
+ }
+
+ printf("\n");
+}
+
+struct node* get_element(int value)
+{
+    struct node* ptr = (struct node*) malloc (sizeof(struct node));
+    //Allocated in the right way
+    if(ptr != NULL)
+    {
+        ptr->value = value;
+        ptr->next = NULL;
+    }
+    return ptr;
+}
+
+struct node* get_tail(struct node* head)
+{
+    if(head == NULL)return NULL;
+
+    struct node* iter = head; //iterator
+
+    while (iter->next != NULL)
+    {
+        iter = iter->next;
+    }
+
+    return iter;
+}
+
+struct node* add_front(struct node* head, int value)
+{
+    struct node* element = get_element(value);
+    //Not Allocated in the right way
+    if(element == NULL) return head;
+
+    element->next = head;
+
+    return element;
+}
+
+
+
+
+
+struct node* add_back(struct node* head, int value)
+{
+    struct node* element = get_element(value);
+
+    struct node* tail = get_tail(head);
+
+    //Not Allocated in the right way
+    if(element == NULL) return NULL;
+
+    tail->next = element;
+
+    return element;
+}
+
+
+void remove_head(struct node* head){
+    if(head == NULL) return NULL;
+    //set the new head to the next node
+    struct node* temp = head;
+    head = head->next;
+    free(temp);
+
+}
+
+void remove_tail(struct node* head){
+    if(head == NULL) return NULL;
+
+    struct node* prev;
+    struct node* tail;
+    struct node* next;
+    tail = head;
+    /*
+    If head is the Only node so remove it
+    |HEAD| ----> NULL
+    */
+    if(tail->next == NULL)
+    {
+        remove_head(head);
+        return;
+    }
+    prev = head;
+    tail = head->next;
+    /*
+    If 2 Nodes remove Tail
+    |HEAD| ----> |TAIL| ---> NULL
+    */
+    if(tail->next == NULL)
+    {
+        prev->next = NULL;
+        free(tail);
+        return ;
+    }
+    /*
+    If linked list of (size > 2 Nodes)
+    |Prev| -----> |Tail| ----> |Next| ---->
+    */
+    next = tail->next;
+    while(next != NULL)
+    {
+        prev = prev->next;
+        tail = tail->next;
+        next = next->next;
+    }
+    prev->next = NULL;
+    free(tail);
+
+}
+
+
+void remove_element(struct node* head, int value)
+{
+    if(head == NULL) return;
+    //If head is the target node so remove it
+    //and set the new head to the next node
+    if(head->value == value)
+    {
+        remove_head(&head);
+        return;
+    }
+
+    struct node* prev = head;
+    struct node* current = head->next;
+    while(current != NULL)
+    {
+        if(current->value == value)
+        {
+            struct node* temp = current->next;
+            prev->next = temp;
+            free(current);
+            return;
+        }
+    }
+
+}
---
+++ linked_list.h
@@ -0,0 +1,54 @@
+#ifndef LINKED_LIST_H_INCLUDED
+#define LINKED_LIST_H_INCLUDED
+
+struct node
+{
+    int value;
+    struct node* next;
+};
+
+/*
+* Prints the values of the  list from the head node to Tail node
+*/
+void print_list(struct node* head);
+
+/*
+* allocate memory for new node then initializes its value with the input "value"
+* and set its next node to NULL
+*/
+struct node* get_element(int value);
+
+/*
+* Get the tail of the list i.e: last element
+*/
+struct node* get_tail(struct node* head);
+
+/*
+*   Add element to be before the head of the list (i.e: to be the new head)
+*/
+struct node* add_front(struct node* head, int value);
+
+
+/*
+*   Add element to be after the tail of the list (i.e: to be the new tail)
+*/
+struct node* add_back(struct node* head, int value);
+
+
+/*
+* removed head node and the next to head becomes the new head
+*/
+void remove_head(struct node* head);
+
+/*
+*  Remove the tail of the list
+*/
+void remove_tail(struct node* head);
+
+/*
+* Removes the first occurrence of the input value from the list
+* Hint: If the removed node is the head the next to head becomes the new head
+*/
+void remove_element(struct node* head, int value);
+
+#endif // LINKED_LIST_H_INCLUDED
